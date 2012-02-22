4d8a9de49ab86be060439c425a08ccd54b53b7e2
Seif Attar <iam@seifattar.net>
2012-02-22 00:44:59 +0000

    Add test subscriber app to play with

---
+++ src/TestSubscriber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestSubscriber
+{
+    using ZMQ;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(Context context = new Context(1))
+            using (Socket subscriber = context.Socket(SocketType.SUB))
+            {
+                subscriber.Connect("tcp://localhost:5555");
+                subscriber.Subscribe("", Encoding.Unicode);
+
+                string message = "";
+
+                while (true)
+                {
+                    message = subscriber.Recv(Encoding.Unicode);
+                    Console.WriteLine(message);
+                }
+            }
+        }
+    }
+}
